83306ceeef514b2864713050f391598f26ba9675
Jayzher Juaniza <jayzherjuaniza@gmail.com>
2024-04-06 23:26:04 +0800

    Modified Login Routes

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -1,28 +1,27 @@
+import { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { useState, useEffect, useContext } from 'react';
 import UserContext from '../userContext';
 import logo from '../images/logo.png';
 
 export default function AppNavBar() {
-
-  const {user, setUser} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
   return (
-      (user.id === null || user.id === undefined) ?
-      <>
-        <Navbar style={{display: "flex", background: "rgba(1, 1, 1, 0.5)"}} variant="light">
-            <img src={logo} style={{height: "80px"}} className="ms-3" />
-            {/*<Navbar.Brand href="/" className="ms-3 fw-bolder fs-3">HI-TEMP</Navbar.Brand>*/}
-            <Nav className="ms-auto me-5">
-              <Nav.Link href="/Login" className="fs-4 fw-bold">Login</Nav.Link>
-            </Nav>
-        </Navbar>
-      </>
-      :
-      <>
-      
-      </>
+    <Navbar style={{ display: "flex", background: "rgba(1, 1, 1, 0.5)" }} variant="light">
+      <Link to="/">
+        <img src={logo} style={{ height: "80px" }} className="ms-3" alt="Logo" />
+      </Link>
+      {user.id === null || user.id === undefined ? (
+        <Nav className="ms-auto me-5">
+          <Link to="/login" className="nav-link fs-4 fw-bold">Login</Link>
+        </Nav>
+      ) : (
+        <>
+        </>
+      )}
+    </Navbar>
   );
-} 
+}
