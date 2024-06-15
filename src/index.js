caeb19bcff84751d7c91d62cebdec395c1eaf433
Jayzher Juaniza <jayzherjuaniza@gmail.com>
2024-06-15 22:45:08 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SocketProvider } from './SocketProvider';
-import { NotificationProvider } from './NotificationContext';
 
 const root = document.getElementById('root');
 createRoot(root).render(
   <SocketProvider url="https://hi-temp-database.onrender.com">
-    <NotificationProvider>
-      <React.StrictMode>
-        <Router>
-          <App />
-        </Router>
-      </React.StrictMode>
-    </NotificationProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </SocketProvider>
 );
