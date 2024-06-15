9698b1ba21b1005aaecab5c66b306f94a167d57a
Jayzher Juaniza <jayzherjuaniza@gmail.com>
2024-06-16 04:53:53 +0800

    Fixing Notifications(6)

--- src/ChatPlatform/UsersLists.js
+++ src/ChatPlatform/UsersLists.js
@@ -1,19 +1,13 @@
-// src/components/UsersLists.js
 import React from 'react';
 import './Chat.css';
 
-const UsersLists = ({ userList, onSelectUser, chatBoxes }) => {
+const UsersLists = ({ userList, onSelectUser }) => {
   return (
     <div>
       <ul className="user-list">
         {userList.map((user) => (
-          <li
-            className={`${user.Status ? 'Online' : 'Offline'} ${chatBoxes[user._id]?.unread ? 'Unread' : ''}`}
-            key={user._id}
-            onClick={() => onSelectUser(user)}
-          >
-            <p style={{ fontSize: '0.8rem', marginRight: '20px' }}>{user.name}</p>
-            {chatBoxes[user._id]?.unread && <span className="unread-indicator">•</span>}
+          <li className={(user.Status) ? 'Online' : 'Offline'} key={user._id} onClick={() => onSelectUser(user)}>
+            <p style={{fontSize: "0.8rem", marginRight: "20px"}}>{user.name}</p>
           </li>
         ))}
       </ul>
