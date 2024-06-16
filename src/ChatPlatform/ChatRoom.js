310dddf645c52260a0ae210a35e36a61fe1c16e2
Jayzher Juaniza <jayzherjuaniza@gmail.com>
2024-06-17 06:22:54 +0800

    Fix Notif(18)

--- src/ChatPlatform/ChatRoom.js
+++ src/ChatPlatform/ChatRoom.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { useSocket } from '../SocketProvider'; // Import useSocket hook from SocketProvider
+import { useSocket } from '../SocketProvider';
 import UsersLists from './UsersLists';
 import ChatBox from './ChatBox';
 import { useNavigate } from 'react-router-dom';
-import { useNotification } from '../NotificationContext'; // Adjust the path as needed
+import { useNotification } from '../NotificationContext';
 import '../components/Style.css';
 import './Chat.css';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const ChatRoom = () => {
-  const socket = useSocket(); // Get the socket instance using useSocket hook
+  const socket = useSocket();
   const { showNotification } = useNotification();
   const [userList, setUserList] = useState([]);
   const [chatBoxes, setChatBoxes] = useState({});
-  const [showUsers, setShowUsers] = useState(true); // Add state to control the visibility of the user list
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 700); // State to track if the device is mobile
+  const [showUsers, setShowUsers] = useState(true);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,7 +24,7 @@ const ChatRoom = () => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 700);
       if (window.innerWidth >= 700) {
-        setShowUsers(true); // Show user list if screen width is greater than or equal to 700px
+        setShowUsers(true);
       }
     };
 
@@ -34,7 +36,7 @@ const ChatRoom = () => {
   }, []);
 
   const fetchUserList = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/users/alldetails`)
+    fetch(`${apiUrl}/users/alldetails`)
       .then((response) => {
         if (!response.ok) {
           throw new Error('Failed to fetch user list');
@@ -72,13 +74,16 @@ const ChatRoom = () => {
   }, [socket]);
 
   const handleNewMessage = (newMessage) => {
-    console.log('New message received:', newMessage);
     const senderName = newMessage.sender?.name || 'Unknown';
     showNotification(`New message from ${senderName}`);
+    setChatBoxes((prevChatBoxes) => ({
+      ...prevChatBoxes,
+      [newMessage.sender.id]: true, // Open the chat box of the sender automatically
+    }));
   };
 
   const handleNotificationClick = () => {
-    navigate('/Messages'); // Navigate to /Messages route
+    navigate('/Messages');
   };
 
   const handleUserSelect = (user) => {
@@ -87,7 +92,7 @@ const ChatRoom = () => {
       [user._id]: !prevChatBoxes[user._id],
     }));
     if (isMobile) {
-      setShowUsers(false); // Hide user list when a user is selected on mobile
+      setShowUsers(false);
     }
   };
 
@@ -97,8 +102,8 @@ const ChatRoom = () => {
   };
 
   const handleBackClick = () => {
-    setShowUsers(true); // Show user list on back button click
-    setChatBoxes({}); // Hide all chat boxes
+    setShowUsers(true);
+    setChatBoxes({});
   };
 
   return (
