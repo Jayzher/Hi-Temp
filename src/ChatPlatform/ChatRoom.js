3ba7dbf5585ba386e39eb1c39ea7a977ea005ac4
Jayzher Juaniza <jayzherjuaniza@gmail.com>
2024-06-16 23:03:24 +0800

    Fixing Notifications(7)

--- src/ChatPlatform/ChatRoom.js
+++ src/ChatPlatform/ChatRoom.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useSocket } from '../SocketProvider'; // Import useSocket hook from SocketProvider
 import UsersLists from './UsersLists';
 import ChatBox from './ChatBox';
-import Notification from './Notification'; // Import Notification component
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../components/Style.css';
 import './Chat.css';
 
@@ -12,8 +13,6 @@ const ChatRoom = () => {
   const [chatBoxes, setChatBoxes] = useState({});
   const [showUsers, setShowUsers] = useState(true); // Add state to control the visibility of the user list
   const [isMobile, setIsMobile] = useState(window.innerWidth < 700); // State to track if the device is mobile
-  const [newMessage, setNewMessage] = useState(null); // State to store new message content
-  const [showNotification, setShowNotification] = useState(false); // State to control notification visibility
 
   useEffect(() => {
     fetchUserList();
@@ -52,22 +51,31 @@ const ChatRoom = () => {
           return updatedChatBoxes;
         });
       })
-      .catch((error) => console.error('Error fetching user list:', error));
+      .catch((error) => {
+        console.error('Error fetching user list:', error);
+        toast.error('Failed to fetch user list');
+      });
   };
 
   useEffect(() => {
     if (socket) {
       // Listen for 'new_message' event using the existing socket instance
       socket.on('new_message', (newMessage) => {
+        // Log the newMessage object to debug
+        console.log('New message received:', newMessage);
+
         // Update messages for the recipient in chatBoxes state
         setChatBoxes((prevChatBoxes) => ({
           ...prevChatBoxes,
-          [newMessage.recipientId]: true,
+          [newMessage.recipient.id]: true,
         }));
 
-        // Set new message and show notification
-        setNewMessage(newMessage.content);
-        setShowNotification(true);
+        // Check if senderName is defined
+        if (newMessage.sender && newMessage.sender.name) {
+          toast.info(`New message from ${newMessage.sender.name}`);
+        } else {
+          toast.info('New message received');
+        }
       });
 
       return () => {
@@ -89,6 +97,7 @@ const ChatRoom = () => {
 
   const handleSendMessage = (recipientId, messageContent) => {
     socket.emit('send_message', { recipientId, content: messageContent });
+    toast.success('Message sent');
   };
 
   const handleBackClick = () => {
@@ -96,26 +105,22 @@ const ChatRoom = () => {
     setChatBoxes({}); // Hide all chat boxes
   };
 
-  const handleCloseNotification = () => {
-    setShowNotification(false);
-    setNewMessage(null);
-  };
-
   return (
     <div className="dashboard-container" style={{ overflow: "hidden", height: '100vh' }}>
+      <ToastContainer />
       <div id="Chatroom-container" className="d-flex flex-row" style={{ height: '100%', maxHeight: '100vh', marginLeft: "15vw", width: '85vw', overflowY: "hidden" }}>
-        <div className="d-flex flex-wrap" style={{ backgroundImage: "linear-gradient(184.1deg, rgba(249,255,182,1) 44.7%, rgba(226,255,172,1) 67.2%)", flex: 1, position: 'relative' }}>
-          {!showUsers && isMobile && (
-            <div className="d-flex align-items-center justify-content-end" style={{ background: "rgba(0, 0, 0, 0.7)", height: "fit-content", width: "100%", padding: "5px", position: 'absolute', top: '0', zIndex: "10"}}>
-              <button onClick={handleBackClick} style={{ color: 'white', border: 'none', background: 'transparent', cursor: 'pointer' }}>
-                &#8592; Back
-              </button>
-            </div>
-          )}
-          <div className="user-list-container" style={{ width: '20%', background: '#f0f0f0'}} hidden={!showUsers} >
-            <h3 className="ms-5 p-2">User List</h3>
-            <UsersLists userList={userList} onSelectUser={handleUserSelect} />
+        {!showUsers && isMobile && (
+          <div className="d-flex align-items-center justify-content-end" style={{ background: "rgba(0, 0, 0, 0.7)", height: "fit-content", width: "100%", padding: "5px", position: 'absolute', top: '0', zIndex: "10"}}>
+            <button onClick={handleBackClick} style={{ color: 'white', border: 'none', background: 'transparent', cursor: 'pointer' }}>
+              &#8592; Back
+            </button>
           </div>
+        )}
+        <div className="user-list-container" style={{ width: '20%', background: '#f0f0f0'}} hidden={!showUsers} >
+          <h3 className="ms-5 p-2">User List</h3>
+          <UsersLists userList={userList} onSelectUser={handleUserSelect} />
+        </div>
+        <div className="d-flex flex-wrap" style={{ backgroundImage: "linear-gradient(184.1deg, rgba(249,255,182,1) 44.7%, rgba(226,255,172,1) 67.2%)", flex: 1, position: 'relative' }}>
           {userList.map((user) => (
             <ChatBox
               key={user._id}
@@ -128,11 +133,6 @@ const ChatRoom = () => {
           ))}
         </div>
       </div>
-      <Notification
-        message={newMessage}
-        show={showNotification}
-        handleClose={handleCloseNotification}
-      />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SocketProvider } from './SocketProvider';
 
+// <SocketProvider url="http://localhost:4000">
+
 const root = document.getElementById('root');
 createRoot(root).render(
   <SocketProvider url="https://hi-temp-database.onrender.com">
