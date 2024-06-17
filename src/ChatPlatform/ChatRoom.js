2e936b4e5fbe1e82032163f3eaf1f50239a9a32a
Jayzher Juaniza <jayzherjuaniza@gmail.com>
2024-06-17 22:44:12 +0800

    Debug Notification(32)

--- src/ChatPlatform/ChatRoom.js
+++ src/ChatPlatform/ChatRoom.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useSocket } from '../SocketProvider'; // Import useSocket hook from SocketProvider
 import UsersLists from './UsersLists';
 import ChatBox from './ChatBox';
